cbe0f4d07a420f9ee75cad77e092d13eb0a34e16
Shreejita26 <112630589+Shreejita26@users.noreply.github.com>
2023-03-20 12:01:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -15,4 +15,45 @@ document.getElementById("result").innerHTML="<img id='captured_image' src='"+dat
  }
  console.log('ml5 version',ml5.version);
 
- classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IqkfODgLO/model.json',modelloaded);
+ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/IqkfODgLO/model.json',modelLoaded);
+
+ function modelLoaded(){
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The prediction is"+prediction_1;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1);
+    synth.speak(utterThis);
+}
+function check(){
+    img= document.getElementById("captured_image");
+    classifier.classify(img,gotResults);
+    }
+    
+    function gotResults(error,results){
+        if(error) {
+        console.error(error);
+       }else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML=results[0].label;
+       
+        prediction_1=results[0].label;
+        
+        speak();
+        if(results[0].label =="great"){
+         document.getElementById("update_gesture").innerHTML="&#128077";
+        }
+        
+        if(results[0].label =="awesome"){
+            document.getElementById("update_gesture").innerHTML="&#128076;";
+        }
+        if(results[0].label=="victory"){
+        document.getElementById("update_gesture").innerHTML="&#9996;";
+        }
+    
+        
+    
+       }
+    }
